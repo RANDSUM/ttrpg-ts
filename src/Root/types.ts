66cce30221637d5a3b14c531f59f15b500a4de9e
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-09-25 23:40:10 -0400

    0.2.3

--- src/Root/types.ts
+++ src/Root/types.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace
-export declare namespace Root {
-  enum RollResult {
+export namespace Root {
+  export enum RollResult {
     success = 'Success',
     partialSuccess = 'Partial Success',
     failure = 'Failure'
