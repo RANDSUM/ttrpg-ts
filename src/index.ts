68221b43c55f697e0aa1405137b460c01d0528b0
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-09-26 00:43:35 -0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // export { default as SalvageUnion } from './SalvageUnion'
 // export { default as DnD5E } from './DnD5E'
 // export { default as Blades } from './Blades'
-import { Root } from './Root/types.ts'
 
 export { default as Root } from './Root'
-export { Root as RootTypes }
+export * from './Root/types'
